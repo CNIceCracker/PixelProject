3c2739f072db7885fcf7e3f1c8fae7d3a5a08fcd
Dragonf1y <543678226@qq.com>
2017-02-05 15:53:50 +0800

    show ammo ver_1.0

--- Assets/Scipt/Entity/Weapon/RangedWeapon.cs
+++ Assets/Scipt/Entity/Weapon/RangedWeapon.cs
@@ -9,7 +9,7 @@ public class RangedWeapon : Weapon {
 	public float accurate;		//精准性
 	public float fireRate;		//每秒攻击频率
 	public int magazineSize;	//弹夹容量
-	public int maxAmmo;			//子弹总数
+	public int maxAmmo;			//子弹上限
 	public float reloadTime;	//填装时间
 
 	public float critChance = 0.05f;	//暴击率
@@ -90,4 +90,11 @@ public class RangedWeapon : Weapon {
 			}
 		}
 	}
+
+    public void GetAmmoInfo( ref int curAmmo,ref int curMagazine)
+    {
+        curAmmo = this.curAmmo;
+        curMagazine = this.curMagazine;
+    }
+    
 }
