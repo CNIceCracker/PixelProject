6c2a5aafac428a5661e9cc55082736bf029d39a1
Dragonf1y <543678226@qq.com>
2017-02-12 20:01:16 +0800

    HPbar_v1.0 & ShowAmmo v2.0

---
+++ Assets/Scipt/UI/PlayerAmmo.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerAmmo : MonoBehaviour {
+    public GameObject player;
+    public int curAmmo;
+    public int curMagazine;
+    public Text AmmoText;
+
+    private RangedWeapon wp;
+	// Use this for initialization
+    void Awake() 
+    {
+        wp = player.GetComponentInChildren<RangedWeapon>();
+        wp.GetAmmoInfo(ref curAmmo, ref curMagazine);
+    }
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        wp.GetAmmoInfo(ref curAmmo, ref curMagazine);
+        AmmoText.text = "Ammo " + curMagazine + "/" + curAmmo;
+        
+	}
+}
---
+++ Assets/Scipt/UI/PlayerHP.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerHP : MonoBehaviour {
+
+    public Slider HPSlider;
+    public float maxHP;
+    public float curHP;
+    public GameObject player;
+	// Use this for initialization
+    void Awake()
+    {
+        curHP = maxHP = player.GetComponent<Fightable>().maxHealth;
+        HPSlider.value = HPSlider.maxValue = maxHP;
+    }
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        curHP = player.GetComponent<Fightable>().GetHealth();
+        HPSlider.value = curHP; 
+	}
+
+}
